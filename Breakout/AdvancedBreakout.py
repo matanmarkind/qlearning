5aa2d7f70f0e4d2a26427be191d393dd56a24e05
Matan Markind <matan.markind@gmail.com>
2018-07-19 09:41:45 +0300

    Breakout - create AdvancedBreakoutQnet. For the moment is just running as a test of WeightedExpBuf. Want to compare if using the weighted experiences makes any difference versus a plain ringbuf before adding other features.

--- Breakout/AdvancedBreakout.py
+++ Breakout/AdvancedBreakout.py
@@ -1,17 +1,15 @@
 """
-This is an attempt to create a more advanced Q net to beat Breakout. Still
-based largely on the deepmind paper, but trying to work in some other
-features
-    - Huber loss
-    - Weighted experience replay
-    - Double DQN
-    - Deuling networks
-    - Updated reward function
-    - Adamoptimizer
+This is an attempt to recreate the algorithm that was used by deepmind in the
+first major paper they published about beating atari games.
+https://www.cs.toronto.edu/~vmnih/docs/dqn.pdf
+
+Uses some changes suggested in
+https://becominghuman.ai/lets-build-an-atari-ai-part-1-dqn-df57e8ff3b26
+
+And the gaps were filled based on the implementation in
+https://github.com/boyuanf/DeepQLearning/blob/master/deep_q_learning.py
 """
 
-from BaseReplayQnet import  BaseReplayQnet
-from ExperienceBuffer import WeightedExpBuf
 from datetime import datetime
 from resource import getrusage, RUSAGE_SELF
 
@@ -20,22 +18,32 @@ import numpy as np
 
 import gym, os, argparse, sys, time
 
+parent_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+if parent_dir not in sys.path:
+    sys.path.insert(1, parent_dir)
+from utils.ExperienceBuffer import WeightedExpBuf
+from utils.BaseReplayQnet import BaseReplayQnet
+
 parser = argparse.ArgumentParser()
 
-parser.add_argument('--mode', type=str, default='show',
-                    help='train, show')
+parser.add_argument('--mode', type=str, help='train, show')
+# TODO: consider having a 2 step anneal. Here we stop at 10% but that may
+# make long terms planning hard for the network since the further into
+# the future we go, the more likely its planning is to get messed up
+# by a forced random action. Perhaps do 100% -> 10% over X steps, then
+# hold random action at 10% for X steps, then anneal from 10% -> 1%
+# over another X steps.
 parser.add_argument('--e_i', type=float, default=1,
                     help="Initial chance of selecting a random action.")
 parser.add_argument('--e_f', type=float, default=.1,
                     help="Final chance of selecting a random action.")
 parser.add_argument(
     '--e_anneal', type=int, default=int(1e6),
-    help='Number of updatews to linearly anneal from eps_i to eps_f.')
+    help='Number of updates to linearly anneal from e_i to e_f.')
 parser.add_argument(
     '--ckpt_dir', type=str,
-    default=os.path.join('..', 'models', 'Breakout'),
     help='Folder to save checkpoints to.')
-parser.add_argument('--ckpt_path', type=str,
+parser.add_argument('--restore_ckpt', type=str,
                     help='path to restore a ckpt from')
 parser.add_argument(
     '--exp_capacity', type=int, default=int(3e5),
@@ -49,8 +57,13 @@ parser.add_argument(
 parser.add_argument(
     '--output_period', type=int, default=1000,
     help='Number of episodes between outputs (print, checkpoint)')
-parser.add_argument('--future_reward_discount', type=float, default=.99,
-                    help="Exponential decay rate of future rewards")
+parser.add_argument(
+    '--learning_rate', type=float, default=1e-3,
+    help="learning rate for the network. passed to the optimizer.")
+parser.add_argument(
+    '--future_discount', type=float, default=0.99,
+    help="Rate at which to discount future rewards.")
+
 
 def preprocess_img(img):
     """
@@ -67,27 +80,19 @@ def normalize(states):
     """
 
     :param states: numpy array of states
-    :return:
+    :return: normalized img with values on [-1, 1)
     """
-    return states.astype(np.float32) / 255.
+    return states.astype(np.float32) / 128. - 1
 
-class MatanBreakoutQnet(BaseReplayQnet):
+class AdvancedBreakoutQnet(BaseReplayQnet):
     """
     Class to perform basic Q learning
     """
-    def __init__(self, input_shape, n_actions, batch_size,
-                 optimizer, exp_buf_capacity, update_target_net_rate, discount):
+    def __init__(self, input_shape, n_actions, batch_size, optimizer,
+                 exp_buf_capacity, discount):
         BaseReplayQnet.__init__(
-            self, input_shape, n_actions, batch_size, optimizer, exp_buf_capacity,
-            WeightedExpBuf, discount)
-
-        self.replay_count = 0  # Number of experiences replayed
-        self.update_target_net_rate = update_target_net_rate
-
-        # This NN evaluates the value of the action selected by the
-        # main_net. This is the secondary net that will be updated by copying
-        # from the main_net every tau updates (tau = update_target_net)
-        self.target_net = self.make_nn('target_net')
+            self, input_shape, n_actions, batch_size, optimizer,
+            WeightedExpBuf(exp_buf_capacity), discount)
 
     def make_nn_impl(self):
         """
@@ -96,43 +101,28 @@ class MatanBreakoutQnet(BaseReplayQnet):
         applied to the final output.
         :return:
         """
-        # Use a convolutional network to analyze the state which is a set of
-        # normalized images.
         initializer = tf.contrib.layers.xavier_initializer
-        conv1 = tf.layers.conv2d(self.state_input, 32, (8, 8), (4, 4),
+        print('state_input', self.state_input)
+        conv1 = tf.layers.conv2d(self.state_input, 16, (8, 8), (4, 4),
                                  activation=tf.nn.relu,
                                  kernel_initializer=initializer(),
                                  bias_initializer=initializer())
-        conv2 = tf.layers.conv2d(conv1, 64, (4, 4), (2, 2),
+        print('conv1', conv1)
+        conv2 = tf.layers.conv2d(conv1, 32, (4, 4), (2, 2),
                                  activation=tf.nn.relu,
                                  kernel_initializer=initializer(),
                                  bias_initializer=initializer())
-        conv3 = tf.layers.conv2d(conv2, 64, (3, 3), (1, 1),
+        print('conv2', conv2)
+        hidden = tf.layers.dense(tf.layers.flatten(conv2), 256,
                                  activation=tf.nn.relu,
                                  kernel_initializer=initializer(),
                                  bias_initializer=initializer())
-        hidden = tf.layers.dense(tf.layers.flatten(conv3), 512,
-                                 activation=tf.nn.relu,
-                                 kernel_initializer=initializer(),
-                                 bias_initializer=initializer())
+        print('hidden', hidden)
+        return tf.layers.dense(hidden, self.n_actions,
+                               kernel_initializer=initializer(),
+                               bias_initializer=initializer())
 
-        # Dueling Qnet. This allows the netowrk to learn the value of being in
-        # a given state and separately to learn which action taken in that state
-        # will be the most advantageous.
-        v, a = tf.split(hidden, 2, 1)
-        value = tf.layers.dense(inputs=v, units=1,
-                                kernel_initializer=initializer(),
-                                bias_initializer=initializer())
-        advantage = tf.layers.dense(inputs=a, units=self.n_actions,
-                                    kernel_initializer=initializer(),
-                                    bias_initializer=initializer())
-
-        # advantage is used to determine the relative advantage of any move.
-        # value is the base value of just being in this state.
-        adv_offset = tf.reduce_mean(advantage, axis=1, keepdims=True)
-        return value + advantage - adv_offset
-
-    def loss(self, expected, actual):
+    def loss_fn(self, expected, actual):
         """
         A function for calculating the loss of the neural network. Common
         examples include RootMeanSquare or HuberLoss.
@@ -140,7 +130,9 @@ class MatanBreakoutQnet(BaseReplayQnet):
         :param actual: a batch of ouputs from the network
         :return: a batch of losses.
         """
-        return tf.reduce_mean(tf.square(expected - actual))
+        return tf.losses.mean_squared_error(
+            labels=expected, predictions=actual,
+            reduction=tf.losses.Reduction.NONE)
 
     def update(self, sess):
         """
@@ -148,10 +140,8 @@ class MatanBreakoutQnet(BaseReplayQnet):
         memory and replaying them.
         :param sess: tf.Session()
         """
-        self.replay_count += self.batch_size
-
         # Get a batch of past experiences.
-        exp_ids, states, actions, rewards, next_states, not_terminals = \
+        ids, states, actions, rewards, next_states, not_terminals = \
             self.exp_buf.sample(self.batch_size)
         states = normalize(states)
         next_states = normalize(next_states)
@@ -162,35 +152,38 @@ class MatanBreakoutQnet(BaseReplayQnet):
         next_actions = self.predict(sess, next_states)
         # Calculate the Q value for each of the next_states, and take the Q
         # value of the action we would take for each next_state.
-        fullQ = sess.run(self.target_net,
+        fullQ = sess.run(self.main_net,
                          feed_dict={self.state_input: next_states})
-        nextQ = fullQ[:, next_actions]
+        nextQ = fullQ[range(self.batch_size), next_actions]
 
         # Discounted future value:
-        # If this is a terminal term: trueQ = r
-        # else: trueQ = r + discount * Q(next_state, next_action)
+        # trueQ = r + discount * Q(next_state, next_action)
+        # If this is a terminal term, trueQ = r
         target_vals = rewards + not_terminals * self.discount * nextQ
 
         # Calculate the value of being back in state and performing action. Then
         # compare that the the expected value just calculated. This is used to
-        # compute the error for feedback.
-        loss, _ = sess.run([self.loss, self.train_op],
-                           feed_dict={
-                               self.state_input: states,
-                               self.taken_actions_input: actions,
-                               self.target_vals_input: target_vals})
+        # compute the error for feedback. Then backpropogate the loss so that
+        # the network can update.
 
-        # TODO: verify loss is in the same order as exp_ids
-        self.exp_buf.update_weights(exp_ids, loss)
+        _ = sess.run(self.train_op,
+                     feed_dict={
+                         self.state_input: states,
+                         self.action_input: actions,
+                         self.target_vals_input: target_vals})
 
-        self.update_target_net()
-
-    def update_target_net(self):
-        if self.replay_count < self.update_target_net_rate:
-            return
-        self.replay_count = 0
-        # TODO: Copy main_net to target_net
-
+        # Now recalculate the loss of the network to update the weights of the experiences.
+        next_actions = self.predict(sess, next_states)
+        fullQ = sess.run(self.main_net,
+                         feed_dict={self.state_input: next_states})
+        nextQ = fullQ[range(self.batch_size), next_actions]
+        target_vals = rewards + not_terminals * self.discount * nextQ
+        loss = sess.run(self.loss,
+                        feed_dict={
+                         self.state_input: states,
+                         self.action_input: actions,
+                         self.target_vals_input: target_vals})
+        self.exp_buf.update_weights(ids, loss)
 
 def play_episode(args, sess, env, qnet, e):
     """
@@ -209,34 +202,37 @@ def play_episode(args, sess, env, qnet, e):
     reward = 0  # total reward for this episode
     turn = 0
 
-
     while not done:
         action = qnet.predict(sess, normalize(np.array([state])))[0]
         if np.random.rand(1) < e:
             action = qnet.rand_action()
 
-        img, r, done, _ = env.step(action + 1) # 0 & 1 don't do anything
+        img, r, done, _ = env.step(action + 1) # {1, 2, 3}
         img = np.reshape(preprocess_img(img), (105, 80, 1))
         next_state = np.concatenate((state[:, :, :3], img), axis=2)
         # TODO: r += info['ale.lives'] - old_lives??
         # Feels weird to clip the reward, but comes from Deepmind paper...
         qnet.add_experience(state, action, np.clip(r, -1, 1), next_state, done)
 
-        if turn % (qnet.batch_size // 8) == 0 and qnet.exp_buf_size() > args.begin_updates:
+        if qnet.exp_buf_size() > args.begin_updates:
             # Once we have enough experiences in the buffer we can
-            # start learning. We want to use each experience on average 8 times
-            # so that is why for a batch size of 8 we would update every turn.
-            qnet.update(sess)
+            # start learning.
+            if turn % (qnet.batch_size // 8) == 0:
+                # We want to use each experience on average 8 times so
+                # that's why for a batch size of 8 we would update every turn.
+                qnet.update(sess)
             if e > args.e_f:
+                # Reduce once for every update on 8 states. This makes e
+                # not dependent on the batch_size.
                 e -= (args.e_i - args.e_f) / args.e_anneal
 
         state = next_state
         reward += r
         turn += 1
 
-    return reward, e
+    return reward, e, turn
 
-def maybe_output(args, sess, saver, qnet, episode, e, rewards):
+def maybe_output(args, sess, saver, qnet, episode, e, rewards, turn):
     """
     Periodically we want to create some sort of output (printing, saving, etc...).
     This function does that.
@@ -255,22 +251,30 @@ def maybe_output(args, sess, saver, qnet, episode, e, rewards):
         return
 
     # Print info about the state of the network
-    exp_buf_size = qnet.exp_buf_size()
-    exp_buf_capacity = qnet.exp_buf_capacity()
-    turn_str = \
-        ' turn=' + str(exp_buf_size) if exp_buf_size < exp_buf_capacity else ''
-    e_str = '' if e > args.e_f else str(e)
+    turn_str =' turn=' + str(turn)
+    e_str = ' e={:0.2f}'.format(e)
     mem_usg_str = \
         ' mem_usage={:0.2f}GB'.format(getrusage(RUSAGE_SELF).ru_maxrss / 2**20)
     print(datetime.now().strftime("%Y-%m-%d %H:%M:%S "), mem_usg_str,
           ' episode=', episode+1,
           ' reward_last_' + str(args.output_period) + '_games=',
-          int(sum(rewards[-args.output_period:])), turn_str, e_str,
+          int(sum(rewards[-args.output_period:])), e_str, turn_str,
           sep='')
 
     # save the model
-    model_name = 'model-deepmind-' + str(episode+1) + '.ckpt'
+    model_name = 'model-AdvancedBreakout-' + str(episode+1) + '.ckpt'
     saver.save(sess, os.path.join(args.ckpt_dir, model_name))
+
+def get_qnet(args, scope=''):
+    """
+    Wrapper for getting the Gridworld network so don't have to copy and paste
+    the same params each time.
+    """
+    with tf.variable_scope(scope, reuse=tf.AUTO_REUSE):
+        return AdvancedBreakoutQnet(
+            input_shape = (105, 80, 4), n_actions=3, batch_size=args.batch_size,
+            optimizer=tf.train.RMSPropOptimizer(.00025, decay=.95, epsilon=.01),
+            exp_buf_capacity=args.exp_capacity, discount=args.future_discount)
 
 def train(args):
     """
@@ -282,10 +286,7 @@ def train(args):
     """
     env = gym.make('BreakoutDeterministic-v4')
     tf.reset_default_graph()
-    qnet = DeepmindBreakoutQnet(
-        input_shape = (105, 80, 4), n_actions=3, batch_size=args.batch_size,
-        optimizer=tf.train.RMSPropOptimizer(.00025, decay=.95, epsilon=.01),
-        exp_buf_capacity=args.exp_capacity)
+    qnet = get_qnet(args)
 
     init = tf.global_variables_initializer()
     saver = tf.train.Saver()
@@ -296,49 +297,57 @@ def train(args):
         e = args.e_i
         episode = 0
         rewards = []
+        turn = 0
 
         while True:
-            r, e = play_episode(args, sess, env, qnet, e)
+            r, e, t = play_episode(args, sess, env, qnet, e)
+            turn += t
             rewards.append(r)
 
             episode += 1
-            maybe_output(args, sess, saver, qnet, episode, e, rewards)
+            maybe_output(args, sess, saver, qnet, episode, e, rewards, turn)
 
 def show_game(args):
     env = gym.make('BreakoutDeterministic-v4')
     tf.reset_default_graph()
-    qnet = DeepmindBreakoutQnet(
-        input_shape = (105, 80, 4), n_actions=3, batch_size=args.batch_size,
-        optimizer=tf.train.RMSPropOptimizer(.00025, decay=.95, epsilon=.01),
-        exp_buf_capacity=args.exp_capacity)
+    qnet = get_qnet(args)
 
-
-    init = tf.global_variables_initializer()
     saver = tf.train.Saver()
     tf.get_default_graph().finalize()
     with tf.Session(config=tf.ConfigProto(operation_timeout_in_ms=10000)) as sess:
-        saver.restore(sess, args.ckpt_path)
+        saver.restore(sess, args.restore_ckpt)
         done = False
         img = preprocess_img(env.reset())
         _ = env.render()
         state = np.stack((img, img, img, img), axis=2)
+        reward, turns = 0, 0
 
         while not done:
             time.sleep(.25)
             action = qnet.predict(sess, normalize(np.array([state])))[0]
 
-            img, r, done, _ = env.step(action + 1) # 0 and 1 don't do anything
+            img, r, done, _ = env.step(action + 1) # {1, 2, 3}
             _ = env.render()
             img = np.reshape(preprocess_img(img), (105, 80, 1))
             state = np.concatenate((state[:, :, :3], img), axis=2)
+            reward += r
+            turns += 1
+
+    print('turns =', turns, ' reward =', reward)
 
 def main():
     args = parser.parse_args(sys.argv[1:])
     if args.mode == 'show':
-        assert args.ckpt_path != '', 'Must provide a checkpoint to show.'
+        assert args.restore_ckpt != '', 'Must provide a checkpoint to show.'
+        args.exp_capacity = 0
         show_game(args)
     elif args.mode == 'train':
+        assert args.ckpt_dir != '', \
+            'Must provide a directory to save checkpoints to.'
         train(args)
+    else:
+        assert False, "Must provide a mode to run in: train, show."
 
 if __name__ == '__main__':
     main()
+
