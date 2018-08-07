31e8069872e78910a813271f6b2188f0b1cf428b
Matan Markind <matan.markind@gmail.com>
2018-08-07 19:18:05 +0300

    RingBuffer - reformat class methods to be grouped as {__init__, public methods, properties, private methods, __fn__}. Also WeightedRingBuf now tracks the non-zero min_weight. 0 is a signifier of a 'dead' leaf so it is ignored. max_weight isn't tracked for 2 reasons: I don't care about it and it would trigger more O(size) searches since by definition the max_weight element is the most likely to be selected (and updated based on my use case).

--- utils/RingBuffer.py
+++ utils/RingBuffer.py
@@ -10,6 +10,7 @@ class RingBuf:
         self.capacity = int(capacity)
         self.index = 0
         self.data = [None] * self.capacity
+        self.iter_index = 0
 
     def append(self, element):
         """
@@ -58,7 +59,23 @@ class RingBuf:
         """
         Allow user to iterate over the elements in the buffer.
         """
-        return self.data.__iter__()
+        return self
+
+    def __next__(self):
+        """
+        Check if there are any valid elements left to return. If not, either
+        have reached the last element in the buffer or have reached None (aka
+        element that has never been set) raise StopIteration and reset the
+        iteration counter.
+
+        :return: single element from the buffer
+        """
+        if self.iter_index >= self.capacity or self[self.iter_index] is None:
+            self.iter_index = 0
+            raise StopIteration
+        else:
+            self.iter_index += 1
+            return self[self.iter_index - 1]
 
 
 class WeightedRingBuf():
@@ -69,32 +86,18 @@ class WeightedRingBuf():
 
     Implemented as a binary tree where each node holds the sum of the weights
     of its children.
+
+    Elements don't even need to be all the same so long as all have weight
+    property which returns a positive number.
     """
 
     def __init__(self, capacity):
         self.capacity = capacity
         self.tree = self.make_tree(capacity)
+        self.min_weight = float("inf")
 
-
-    def append(self, ele):
-        """
-        Appends an element to the buffer and updates the weights of its
-        parent nodes.
-        :param ele:
-        :return: old element
-        """
-        weight = ele.weight
-        ele.weight = 0
-        idx, old_ele = self.tree[-1].append(ele)
-        old_weight = 0 if old_ele is None else old_ele.weight
-        
-        self._update_weight(idx, weight - old_weight)
-        # Must reset ele.weight since now it is (weight - old_weight)
-        ele.weight = weight
-        
-        return idx, old_ele
-
-    def make_tree(self, capacity):
+    @staticmethod
+    def make_tree(capacity):
         """
         Create a tree with all weights initialized to 0.
         """
@@ -106,48 +109,35 @@ class WeightedRingBuf():
         tree.append(RingBuf(capacity))
         return tree
 
-    def get_leaf(self, min_weight=0, max_weight=None):
+    def append(self, ele):
         """
-        Randomly select a leaf from the tree based on the weights in the tree.
-        Returns the id of the leaf (the index).
+        Appends an element to the buffer and updates the weights of its
+        parent nodes. If the weight of the element is 0, will not update
+        min_weight.
+        :param ele: object with weight property (ele.weight returns a number).
+        :return: old element
         """
-        max_weight = self.total_weight if max_weight is None else max_weight
-        assert min_weight >= 0.0 and min_weight < max_weight and \
-                max_weight <= self.total_weight,\
-                "Invalid weight subsetting"
+        weight = ele.weight
+        ele.weight = 0
+        idx, old_ele = self.ring_buf.append(ele)
+        old_weight = 0 if old_ele is None else old_ele.weight
+        
+        self._update_weight(idx, weight - old_weight)
+        # Must reset ele.weight since now it is (weight - old_weight)
+        ele.weight = weight
 
-        val = random.uniform(min_weight, max_weight)
-        idx = 0
-        for depth in range(1, len(self.tree) - 1):
-            left_weight = self.tree[depth][idx]
-            if val >= left_weight:
-                val -= left_weight
-                idx = (idx + 1) * 2
-            else:
-                idx *= 2
-        left_weight = self[idx].weight
-        return idx + (val >= left_weight)
-    
+        self._update_min_weight(old_weight, weight)
+        return idx, old_ele
+
     def update_weight(self, index, weight):
         """
         Reset the weight of an element and update the weights in the tree.
         """
         idx = index % self.capacity
-        delta = weight - self[idx].weight
+        old_weight = self[idx].weight
+        delta = weight - old_weight
         self._update_weight(idx, delta)
-
-    def _update_weight(self, idx, delta):
-        """
-        Resets the weight of the element identified by index, then
-        goes up each row updating the parent nodes with the new weight.
-        :param idx: index of the changed experience.
-        :param delta: change in weight of the element.
-        """
-        self[idx].weight += delta
-        idx //= 2
-        for depth in range(-2, -len(self.tree) - 1, -1):
-            self.tree[depth][idx] += delta
-            idx //= 2
+        self._update_min_weight(old_weight, weight)
 
     def sample(self, n, exclude=set()):
         """
@@ -157,8 +147,10 @@ class WeightedRingBuf():
         aren't any really extreme outliers that represent a significant
         portion of the total_weight (I'm gunna ballpark and say so long as
         no element is > total_weight/n we should be ok).
+
         :param n: how many experiences to sample.
         :param exclude: excluce certain elements, handles wraparound.
+
         Returns a unique set of indices for the leaves in the tree.
         """
         exclude_idx = set([index % self.capacity for index in exclude])
@@ -192,6 +184,14 @@ class WeightedRingBuf():
             min_weight += weight_step
         return idxs
 
+    @property
+    def total_weight(self):
+        return self.tree[0][0]
+
+    @property
+    def ring_buf(self):
+        return self.tree[-1]
+
     def _sample(self, exclude_idx=set(), min_weight=0.0, max_weight=None):
         """
         Sample a single element which falls within a subset of the trees
@@ -203,20 +203,80 @@ class WeightedRingBuf():
             subsection of the tree.
         """
         while True:
-            leaf = self.get_leaf(min_weight, max_weight)
+            leaf = self._get_leaf(min_weight, max_weight)
             if leaf not in exclude_idx:
                 return leaf
 
-    @property
-    def total_weight(self):
-        return self.tree[0][0]
+    def _get_leaf(self, min_weight=0, max_weight=None):
+        """
+        Randomly select a leaf from the tree based on the weights in the tree.
+        Returns the id of the leaf (the index).
 
-    @property
-    def min_weight(self):
-        # O(capacity)
+        :param min_weight: minimum weight to sample from.
+        :param max_weight: maximum weight to sample from.
+        """
+        max_weight = self.total_weight if max_weight is None else max_weight
+        assert min_weight >= 0.0 and min_weight < max_weight and \
+                max_weight <= self.total_weight,\
+                "Invalid weight subsetting"
+
+        val = random.uniform(min_weight, max_weight)
+        idx = 0
+        for depth in range(1, len(self.tree) - 1):
+            left_weight = self.tree[depth][idx]
+            if val >= left_weight:
+                val -= left_weight
+                idx = (idx + 1) * 2
+            else:
+                idx *= 2
+        left_weight = self[idx].weight
+        return idx + (val >= left_weight)
+
+    def _update_weight(self, idx, delta):
+        """
+        Resets the weight of the element identified by index, then
+        goes up each row updating the parent nodes with the new weight.
+        :param idx: index of the changed experience.
+        :param delta: change in weight of the element.
+        """
+        self[idx].weight += delta
+        idx //= 2
+        for depth in range(-2, -len(self.tree) - 1, -1):
+            self.tree[depth][idx] += delta
+            idx //= 2
+
+    def _update_min_weight(self, old_weight, weight):
+        """
+        Updates the minimum weight for the buffer. Used whenever an
+        element has its weight updated or is replaced. 
+
+        If old weight is 0, guarantees not to trigger a find()
+        """
+        if weight == 0:
+            # A weight of 0 is an indicator of an invalid element since it is
+            # unreachable via sampling.
+            return
+
+        if weight < self.min_weight:
+            self.min_weight = weight
+        elif old_weight == self.min_weight:
+            # If the previous min_weight was replaced, and the new weight
+            # isn't the replacement, we must search all elements for the
+            # new minimum weight.
+            self.min_weight = self._find_min_weight()
+
+    def _find_min_weight(self):
+        """
+        Find the element with the minimum weight in the buffer and return
+        its weight. This ignores elements with 0 weight, since they
+        are unreachable via sampling.
+
+        O(capacity), but given that sampling is done in proportion to the
+        weights, this should be called quite rarely.
+        """
         min_weight = float("inf")
-        for ele in self.tree[-1]:
-            if ele.weight < min_weight:
+        for ele in self.ring_buf:
+            if ele.weight != 0 and ele.weight < min_weight:
                 min_weight = ele.weight
         return min_weight
 
@@ -226,7 +286,7 @@ class WeightedRingBuf():
         sit at the base of the tree in a RingBuf. That is what tells us the
         effective size/number of elements.
         """
-        return len(self.tree[-1])
+        return len(self.ring_buf)
 
     def __getitem__(self, idx):
         """
@@ -234,10 +294,10 @@ class WeightedRingBuf():
         :param idx: idx of the element.
         :return:
         """
-        return self.tree[-1][idx]
+        return self.ring_buf[idx]
 
     def __iter__(self):
         """
         Allow user to iterate over the elements in the buffer.
         """
-        return self.tree[-1].__iter__()
+        return self.ring_buf.__iter__()
